327b87c2446a6d55c1a35e0002392f446f726df1
z0ckr3 <z0ckr3@hotmail.com>
2022-10-13 11:56:14 -0400

    Initial Commit

---
+++ vars/dockerLib.groovy
@@ -0,0 +1,15 @@
+def build(Map params) {
+    sh "docker build -f ${params.DockerfilePath} -t ${params.DockerImage} ${params.DockerContext}"
+}
+
+def login(Map params) {
+    sh "${params.DockerPass} | docker login -u ${params.DockerUser} --password-stdin"
+}
+
+def push(Map params) {
+    sh "docker push ${params.DockerImage}"
+}
+
+def logout() {
+    sh 'docker logout'
+}
